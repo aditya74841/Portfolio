2b7acb572bb9d7cabee2d8fb52eb2da52c92f36c
Aditya Ranjan <aditya74810@gmail.com>
2022-04-14 23:04:51 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const buton = document.querySelector("#buton");
+const black = document.querySelector("#black");
+
+
+buton.addEventListener("click", () => {
+    console.log("Aditya Ranjan")
+  document.body.style.background = "white";
+});
+black.addEventListener("click", () => {
+    console.log("Aditya Ranjan")
+  document.body.style.background = "black";
+});
+
